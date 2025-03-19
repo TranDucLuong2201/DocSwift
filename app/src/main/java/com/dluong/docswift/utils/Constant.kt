35fda63b6fd92a652a88d1b2231de4159cc9020e
ducluong221 <tducluong.individual@gmail.com>
2025-03-19 11:14:25 +0700

    delete web client id

--- app/src/main/java/com/dluong/docswift/utils/Constant.kt
+++ app/src/main/java/com/dluong/docswift/utils/Constant.kt
@@ -5,7 +5,7 @@ object Constant {
     const val ON_BOARDING_PAGE_COUNT = 3
     const val FIRST_INDEX_PAGE = 0
 
-    const val WEB_CLIENT_ID = "253037230551-ghsoc18vjnbe5ithe4eelbbijvupan8g.apps.googleusercontent.com"
+    const val WEB_CLIENT_ID = "client id"
 
     const val MAX_LINE_TEXT = 1
 }
